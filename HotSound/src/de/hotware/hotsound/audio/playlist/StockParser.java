8319695e076cf9e9b5b91dc01cce6a998cd8b0cb
Martin Braun <martinbraun123@aol.com>
2012-09-24 20:13:43 +0200

    fixed import

--- HotSound/src/de/hotware/hotsound/audio/playlist/StockParser.java
+++ HotSound/src/de/hotware/hotsound/audio/playlist/StockParser.java
@@ -3,7 +3,6 @@ package de.hotware.hotsound.audio.playlist;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
