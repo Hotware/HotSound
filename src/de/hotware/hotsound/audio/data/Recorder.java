ff034846763e75584162a5106dc14d4864d0f0a3
Martin Braun <martinbraun123@aol.com>
2012-09-27 23:43:26 +0200

    the recorder now saves the right amount of bytes

--- src/de/hotware/hotsound/audio/data/Recorder.java
+++ src/de/hotware/hotsound/audio/data/Recorder.java
@@ -1,7 +1,9 @@
 package de.hotware.hotsound.audio.data;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -9,26 +11,30 @@ import javax.sound.sampled.AudioFormat;
 
 import de.hotware.hotsound.audio.data.IAudioDevice.AudioDeviceException;
 
-
 public class Recorder implements AutoCloseable {
-	
+
 	private BufferedOutputStream mBufferedOutputStream;
 	private File mFile;
+	private File mTempFile;
 	private IHeader mHeader;
+	private int mBytesWritten;
 
 	public Recorder(File pFile) {
 		if(pFile == null) {
 			throw new IllegalArgumentException("pFile may not be null");
 		}
 		this.mFile = pFile;
+		this.mTempFile = new File(pFile.getAbsolutePath() + ".tmp");
+		this.mBytesWritten = 0;
 	}
 
-	public void open(AudioFormat pAudioFormat) throws AudioDeviceException, IOException {
-		if(this.mFile.exists()) {
+	public void open(AudioFormat pAudioFormat) throws AudioDeviceException,
+			IOException {
+		if(this.mTempFile.exists()) {
 			this.mFile.delete();
 		}
-		this.mFile.createNewFile();
-		this.mBufferedOutputStream = new BufferedOutputStream(new FileOutputStream(this.mFile));
+		this.mTempFile.createNewFile();
+		this.mBufferedOutputStream = new BufferedOutputStream(new FileOutputStream(this.mTempFile));
 		this.mHeader = new WaveHeader(WaveHeader.FORMAT_PCM,
 				(short) pAudioFormat.getChannels(),
 				(int) pAudioFormat.getSampleRate(),
@@ -39,13 +45,35 @@ public class Recorder implements AutoCloseable {
 
 	public int write(byte[] pData, int pStart, int pLength) throws IOException {
 		this.mBufferedOutputStream.write(pData, pStart, pLength);
+		this.mBytesWritten += pLength;
 		return pLength;
 	}
 
 	public void close() throws IOException {
-		if(this.mBufferedOutputStream != null) {
+		try(BufferedInputStream input = new BufferedInputStream(new FileInputStream(this.mTempFile))) {
+			if(this.mBufferedOutputStream != null) {
+				this.mBufferedOutputStream.flush();
+				this.mBufferedOutputStream.close();
+				;
+				((WaveHeader) this.mHeader).read(input);
+				((WaveHeader) this.mHeader).setNumBytes(this.mBytesWritten);
+				if(this.mFile.exists()) {
+					this.mFile.delete();
+				}
+				this.mFile.createNewFile();
+				this.mBufferedOutputStream = new BufferedOutputStream(new FileOutputStream(this.mFile));
+				int bytesRead = 0;
+				byte[] data = new byte[128000];
+				this.mHeader.write(this.mBufferedOutputStream);
+				while((bytesRead = input.read(data, 0, 128000)) != -1) {
+					this.write(data, 0, bytesRead);
+				}
+			}
+		} finally {
+			this.mTempFile.delete();
 			this.mBufferedOutputStream.flush();
 			this.mBufferedOutputStream.close();
+			this.mBufferedOutputStream = null;
 		}
 	}
 
