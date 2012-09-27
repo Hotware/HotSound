92ee007e09c924b29c14514366199ccf1cf32cdc
Martin Braun <martinbraun123@aol.com>
2012-09-27 17:49:23 +0200

    IPlaybackAudioDevice is used for actual playback devices. the normal interface can play to whatever it likes (even to potatoes if they like the sound of bytes)

--- src/de/hotware/hotsound/audio/data/IPlaybackAudioDevice.java
+++ src/de/hotware/hotsound/audio/data/IPlaybackAudioDevice.java
@@ -3,9 +3,16 @@ package de.hotware.hotsound.audio.data;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.Mixer;
 
-
+/**
+ * IPlaybackAudioDevice is used for actual playback devices. the normal
+ * interface can play to whatever it likes (even to potatoes if they like the
+ * sound of bytes)
+ * 
+ * @author Martin Braun
+ * 
+ */
 public interface IPlaybackAudioDevice extends IAudioDevice {
-	
+
 	public void setMixer(Mixer pMixer);
 
 	/**
